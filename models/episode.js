f29103da197c109dc2bb3140ba99423267d2fba1
Jack Case <jackacase@gmail.com>
2018-03-15 23:24:41 -0400

    episode schema written

---
+++ models/episode.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var EpisodeSchema = new Schema(
+    {
+        title: {type: String, required: true},
+        number: {type: Number, required: true, unique: true},
+        date: {type: Date, required: true},
+        description: {type: String},
+        member: [{type: Schema.ObjectId, ref: 'Member'}],
+        feature: [{type: Schema.ObjectId, ref: 'Feature'}],
+        jingle: [{type: Schema.ObjectId, ref: 'Jingle'}],
+        type: {type: string},
+        tags: [{type: String}]
+    }
+);
+
+EpisodeSchema.virtual('fullTitle').get(function() {
+    return this.number + ": " + this.title;
+});
+
+module.exports = mongoose.model('Episode', EpisodeSchema);
