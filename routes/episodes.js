0fabb936766c6d68e661374780c8ca4af570f4d3
Jack Case <jackacase@gmail.com>
2018-04-16 16:28:49 -0400

    configured episode edit route

--- routes/episodes.js
+++ routes/episodes.js
@@ -7,4 +7,6 @@ router.get('/', episode_controller.episode_list);
 
 router.get('/:id', episode_controller.episode_detail);
 
+router.get('/edit/:id', episode_controller.episode_update);
+
 module.exports = router;
